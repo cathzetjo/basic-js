09a3887dc5da513709703afba41489ea34f73728
Katerina Zhadko <katerinkaj@icloud.com>
2021-10-03 02:57:08 +0300

    feat: add solution for task recursive depth

--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -1,11 +1,9 @@
-import { NotImplementedError } from '../extensions/index.js';
-
 /**
  * Implement class DepthCalculator with method calculateDepth
- * that calculates deoth of nested array
- * 
+ * that calculates depth of nested array
+ *
  * @example
- * 
+ *
  * const depthCalc = new DepthCalculator();
  * depthCalc.calculateDepth([1, 2, 3, 4, 5]) => 1
  * depthCalc.calculateDepth([1, 2, 3, [4, 5]]) => 2
@@ -13,8 +11,14 @@ import { NotImplementedError } from '../extensions/index.js';
  *
  */
 export default class DepthCalculator {
-  calculateDepth(/* arr */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
+    calculateDepth(arr) {
+
+        let array = [];
+        let minLevel = 1;
+        let filter = arr.filter((item) => Array.isArray(item));
+
+        if (filter.length === 0) { return minLevel;}
+        else {return (minLevel + this.calculateDepth(array.concat(...filter)));}
+
+    }
 }
