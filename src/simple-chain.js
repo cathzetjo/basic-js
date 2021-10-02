dd819eeb87c0f5214fa01ada0c8c6a96596b6d27
Katerina Zhadko <katerinkaj@icloud.com>
2021-10-03 02:40:28 +0300

    feat: add solution for task simple chain

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -1,28 +1,40 @@
-import { NotImplementedError } from '../extensions/index.js';
-
 /**
  * Implement chainMaker object according to task description
  * 
  */
 export default {
+  array: [],
+
   getLength() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.array.length;
   },
-  addLink(/* value */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  addLink(value) {
+    this.array.push(value);
+    return this;
   },
-  removeLink(/* position */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  removeLink(position) {
+    const pos = position-1;
+    if (this.array[pos] === undefined) {
+      this.array = [];
+      throw new Error("You can't remove incorrect link!");
+    }
+    else {
+      this.array.splice(pos, 1);
+    }
+    return this;
   },
+
   reverseChain() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    this.array.reverse();
+    return this;
   },
+
   finishChain() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
+    const cainArray = this.array.map((el) => `( ${el} )`);
+    const final=  cainArray.join('~~');
+    this.array = [];
+    return final;
+  },
 };
