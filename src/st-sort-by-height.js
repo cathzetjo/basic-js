de49b738970a9abb710a8773b27c42d3a4c1e163
Katerina Zhadko <katerinkaj@icloud.com>
2021-10-03 20:46:51 +0300

    feat: add solution for task sort by height

--- src/st-sort-by-height.js
+++ src/st-sort-by-height.js
@@ -1,5 +1,3 @@
-import { NotImplementedError } from '../extensions/index.js';
-
 /**
  * Given an array with heights, sort them except if the value is -1.
  *
@@ -11,7 +9,15 @@ import { NotImplementedError } from '../extensions/index.js';
  *
  * The result should be [-1, 150, 160, 170, -1, -1, 180, 190]
  */
-export default function sortByHeight(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function sortByHeight(arr) {
+
+  const notSortedElement = -1;
+  const arrWithoutMinusOne = arr.filter((el) => el !== notSortedElement);
+  const sort = arrWithoutMinusOne.sort((a, b) => a - b);
+  return arr.map((element) => {
+    if (element === notSortedElement) {
+      return element;
+    }
+    return sort.shift();
+  });
 }
