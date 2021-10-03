fe1583ef2564c52c0831a2fbc0cd753fb860bd47
Katerina Zhadko <katerinkaj@icloud.com>
2021-10-03 20:21:34 +0300

    feat: add solution for task delete digit

--- src/st-delete-digit.js
+++ src/st-delete-digit.js
@@ -1,5 +1,3 @@
-import { NotImplementedError } from '../extensions/index.js';
-
 /**
  * Given some integer, find the maximal number you can obtain
  * by deleting exactly one digit of the given number.
@@ -11,7 +9,17 @@ import { NotImplementedError } from '../extensions/index.js';
  * For n = 152, the output should be 52
  *
  */
-export default function deleteDigit(/* n */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function deleteDigit(n) {
+  const string = n+'';
+  const array = string.split('');
+  const min = Math.min.apply(null, array);
+  const index = array.indexOf(min+'');
+  const length = array.length;
+
+  for (let i=0; i<length; i++) {
+    if (i===(index)) {
+      array.splice(index, 1);
+    }
+  }
+  return Number(array.join(''));
 }
